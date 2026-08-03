607c7b547557e798a535b62700e9ba892184b8a0
AnfasArshad <anfasarshad@gmail.com>
2026-08-03 13:47:56 +0530

    Created Comment REST API and Test Create Comment

---
+++ blog-rest-api/src/main/java/com/springboot/blog/blog_rest_api/service/CommentService.java
@@ -0,0 +1,7 @@
+package com.springboot.blog.blog_rest_api.service;
+
+import com.springboot.blog.blog_rest_api.payload.CommentDto;
+
+public interface CommentService {
+    CommentDto createComment(long postId, CommentDto commentDto);
+}
